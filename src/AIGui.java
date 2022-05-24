9a192f73bbf0968fd10897c80daf23b16fd7eeba
zimniok <pierzchala.ma@gmail.com>
2022-05-24 20:34:11 +0200

    Working alpha-beta pruning

--- src/AIGui.java
+++ src/AIGui.java
@@ -22,7 +22,7 @@ public class AIGui extends JFrame {
         showButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                GameBoard gameBoardToShow = Engine.startMinMax(gameGui.gameBoard, 1);
+                GameBoard gameBoardToShow = Engine.startMinMax(gameGui.gameBoard, 5);
                 GameBoardPreview gameBoardPreview = new GameBoardPreview(gameBoardToShow);
             }
         });
@@ -32,15 +32,26 @@ public class AIGui extends JFrame {
         makeMoveButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                GameBoard gameBoardToShow = Engine.startMinMax(gameGui.gameBoard, 1);
+                GameBoard gameBoardToShow = Engine.startMinMax(gameGui.gameBoard, 5);
                 gameGui.gameBoard = gameBoardToShow;
                 gameGui.repaint();
                 System.out.println(gameGui.gameBoard.getCurrentMove());
             }
         });
 
+        JButton showAB = new JButton("Show best Alpha-Beta");
+        showAB.setBounds(30, 130, 200, 20);
+        showAB.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                GameBoard gameBoardToShow = Engine.startAlphaBeta(gameGui.gameBoard, 5);
+                GameBoardPreview gameBoardPreview = new GameBoardPreview(gameBoardToShow);
+            }
+        });
+
         panel.add(showButton);
         panel.add(makeMoveButton);
+        panel.add(showAB);
         this.add(panel);
         this.repaint();
         this.setVisible(true);
--- src/Engine.java
+++ src/Engine.java
@@ -4,6 +4,101 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Engine {
+    public static GameBoard startAlphaBeta(GameBoard gameBoard, int depth){
+        double alpha = Double.MIN_VALUE;
+        double beta = Double.MAX_VALUE;
+        ArrayList<Pair<GameBoard, Double>> firstKids = new ArrayList<>();
+        for (GamePiece gamePiece: gameBoard.getGamePieces()){
+            if(gamePiece.getColor() == gameBoard.getCurrentColorToMove()){
+                for (Pair<Integer, Integer> move: gamePiece.getAvailableMoves()){
+                    GameBoard nextState = new GameBoard(gameBoard);
+                    GamePiece nextStatePiece = nextState.getPiece(gamePiece.getPosX(), gamePiece.getPosY());
+                    Pair<Integer, Integer> beforeMovePos = new Pair<>(nextStatePiece.getPosX(), nextStatePiece.getPosY());
+                    nextStatePiece.move(move.getKey(), move.getValue());
+                    if(gameBoard.getAvailableCapturesCount() > 0) {
+                        nextState.capture(beforeMovePos, move, nextStatePiece);
+                    }else {
+                        nextState.afterMoveCalculations(nextStatePiece);
+                    }
+                    if (gameBoard.getCurrentColorToMove() == GamePiece.WHITE && firstKids.size() > 0 && firstKids.get(firstKids.size()-1).getValue() > alpha)
+                        alpha = firstKids.get(firstKids.size()-1).getValue();
+                    else if(gameBoard.getCurrentColorToMove() == GamePiece.BLACK && firstKids.size() > 0 && firstKids.get(firstKids.size()-1).getValue() < beta)
+                        beta = firstKids.get(firstKids.size()-1).getValue();
+                    double bestChildEval = alphaBeta(nextState, depth, gameBoard.getCurrentColorToMove(), alpha, beta);
+
+                    firstKids.add(new Pair<>(nextState, bestChildEval));
+
+                    if(gameBoard.getCurrentColorToMove() == GamePiece.WHITE && bestChildEval > beta){
+                        break;
+                    } else if(gameBoard.getCurrentColorToMove() == GamePiece.BLACK && bestChildEval < alpha){
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (firstKids.size() == 0)
+            return null;
+
+        int bestValuePos = 0;
+        for (int i = 1; i < firstKids.size(); i++){
+            if(gameBoard.getCurrentColorToMove() == GamePiece.WHITE && firstKids.get(i).getValue() > firstKids.get(bestValuePos).getValue())
+                bestValuePos = i;
+            else if(firstKids.get(i).getValue() < firstKids.get(bestValuePos).getValue())
+                bestValuePos = i;
+        }
+
+        return firstKids.get(bestValuePos).getKey();
+    }
+
+    private static double alphaBeta(GameBoard gameBoard, int depth, int lastMoveColor, double alpha, double beta){
+        ArrayList<Double> evaluations = new ArrayList<>();
+        if(gameBoard.getCurrentColorToMove() != lastMoveColor){
+            depth --;
+        }
+        for(GamePiece gamePiece: gameBoard.getGamePieces()){
+            if (gamePiece.getColor() == gameBoard.getCurrentColorToMove()){
+                for (Pair<Integer, Integer> move: gamePiece.getAvailableMoves()) {
+                    GameBoard nextState = new GameBoard(gameBoard);
+                    GamePiece nextStatePiece = nextState.getPiece(gamePiece.getPosX(), gamePiece.getPosY());
+                    Pair<Integer, Integer> beforeMovePos = new Pair<>(nextStatePiece.getPosX(), nextStatePiece.getPosY());
+                    nextStatePiece.move(move.getKey(), move.getValue());
+                    if(gameBoard.getAvailableCapturesCount() > 0){
+                        nextState.capture(beforeMovePos, move, nextStatePiece);
+                    } else {
+                        nextState.afterMoveCalculations(nextStatePiece);
+                    }
+                    int gameState = nextState.checkGameState();
+                    if(gameState == GameBoard.WHITE_WIN || gameState == GameBoard.BLACK_WIN || gameState == GameBoard.STALEMATE || depth <= 0){
+                        evaluations.add(nextState.evaluate());
+                        continue;
+                    }
+                    int nextLastMoveColor = lastMoveColor;
+                    if(gameBoard.getCurrentColorToMove() != lastMoveColor){
+                        nextLastMoveColor = gameBoard.getCurrentColorToMove();
+                    }
+                    if (lastMoveColor == GamePiece.WHITE && evaluations.size() > 0 && evaluations.get(evaluations.size()-1) > alpha)
+                        alpha = evaluations.get(evaluations.size()-1);
+                    else if(lastMoveColor == GamePiece.BLACK && evaluations.size() > 0 && evaluations.get(evaluations.size()-1) < beta)
+                        beta = evaluations.get(evaluations.size()-1);
+                    double bestChildEval = alphaBeta(nextState, depth, nextLastMoveColor, alpha, beta);
+                    evaluations.add(bestChildEval);
+                    if(lastMoveColor == GamePiece.WHITE && bestChildEval > beta){
+                        break;
+                    } else if(lastMoveColor == GamePiece.BLACK && bestChildEval < alpha){
+                        break;
+                    }
+                }
+            }
+        }
+        if(evaluations.size() == 0)
+            return gameBoard.evaluate();
+        if (lastMoveColor == GamePiece.WHITE){
+            return Collections.max(evaluations);
+        }
+        return Collections.min(evaluations);
+    }
+
     public static GameBoard startMinMax(GameBoard gameBoard, int depth){
         ArrayList<Pair<GameBoard, Double>> firstKids = new ArrayList<>();
         for (GamePiece gamePiece: gameBoard.getGamePieces()){
